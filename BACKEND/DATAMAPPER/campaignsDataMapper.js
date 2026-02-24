c9cec3121d4d662bb311e357f90503e0a359a91b
Kerdjadj Reda <128867058+Kerdjadjreda@users.noreply.github.com>
2026-02-24 20:06:22 +0100

    joined table campaigns and campaign_members. with SQL logic. a user who now creates a campaign will have role: Game Master and will be associated with his id in campaign_members.

--- BACKEND/DATAMAPPER/campaignsDataMapper.js
+++ BACKEND/DATAMAPPER/campaignsDataMapper.js
@@ -2,14 +2,40 @@ const pool = require("../SERVICES/dbPool");
 
 const campaignsDataMapper = {
 
-    async createOne({camp_name, mode, synopsis, creatorUserId}){
-        const sqlQuery = `INSERT INTO campaigns (camp_name, mode, synopsis, creator_user_id)
-                          VALUES ($1, $2, $3, $4)
-                          RETURNING *;`;
-        const values = [camp_name, mode, synopsis, creatorUserId];
-        const result = await pool.query(sqlQuery, values);
-        console.log("teeest", result.rows[0]);
-        return result.rows[0];
+    async createOne({ camp_name, mode, synopsis, creatorUserId }){
+        // je résèrve un pool de connexion pour la transaction à venir.
+        const client = await pool.connect();
+        const role2 = "Maitre du jeu";
+        try {
+            await client.query("BEGIN");
+            // cette requete me permet via les infos de créer une campagne
+            const insertCampaign = await client.query(
+                `INSERT INTO campaigns (camp_name, mode, synopsis, creator_user_id)
+                 VALUES ($1, $2, $3, $4)
+                 RETURNING *;`,
+                 [camp_name, mode, synopsis, creatorUserId]
+                );
+            const campaign = insertCampaign.rows[0];
+            
+            // et là je dois ajouter les informations dans la table pivot
+
+            await client.query(
+                `INSERT INTO campaign_members (campaign_id, user_id, role, joined_at)
+                VALUES ($1, $2, $3, NOW())`,
+                [campaign.id, creatorUserId, role2]
+            );
+            // je termine la transaction avec commit
+            await client.query("COMMIT");
+            return campaign;
+            // en cas d'erreur, j'annule la transaction tout en récupérant l'erreur
+        } catch (error) {
+            await client.query("ROLLBACK");
+            throw error;
+            // et je relache la pool de connexion que j'ai alloué pour la transaction
+        } finally {
+            await client.release();
+        }
+
     },
 
     async countCreatedCampaignByUser(userId){
