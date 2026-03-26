13071fce17f0cfaa1f548bfc04a8a73c54a4f4c6
Kerdjadj Reda <128867058+Kerdjadjreda@users.noreply.github.com>
2026-03-26 16:59:47 +0100

    feat: sql requests to GET all monsters + by his id.

--- BACKEND/DATAMAPPER/monstersDataMapper.js
+++ BACKEND/DATAMAPPER/monstersDataMapper.js
@@ -4,11 +4,14 @@ const pool = require("../SERVICES/dbPool");
 const monstersDataMapper = {
 
     async findAll(){
-
+        const result = await pool.query(`SELECT * FROM monster_templates ORDER BY monster_name ASC;`);
+        return result.rows;
     },
 
-    async findByPk(){
-        
+    async findByPk(monsterId){
+        const result = await pool.query(`SELECT * FROM monster_templates WHERE id =$1`, [monsterId]);
+        return result.rows[0];
+
     }
 }
 
